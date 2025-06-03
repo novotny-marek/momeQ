b2db326c7430e17f1537dd714b263ed2c792b28e
novotny-marek <ahoj@mnovotny.xyz>
2025-06-03 14:15:29 +0200

    utils: Move to_gdf function to utils

--- test_algorithm.py
+++ test_algorithm.py
@@ -13,26 +13,6 @@ from qgis.core import (
     QgsProcessingParameterFeatureSource,
 )
 
-def to_gdf(layer):
-    """Convert QGIS layer to GeoDataFrame"""
-    features = []
-    for feature in layer.getFeatures():
-        geom = feature.geometry()
-        if geom:
-            # Convert QGIS geometry to Shapely geometry
-            geom_wkt = geom.asWkt()
-            shapely_geom = shapely.wkt.loads(geom_wkt)
-            
-            features.append({
-                'geometry': shapely_geom,
-                **{field.name(): feature[field.name()] for field in layer.fields()}
-            })
-    
-    if features:
-        return gpd.GeoDataFrame(features, crs=layer.sourceCrs().authid())
-    else:
-        return gpd.GeoDataFrame()
-
 class test_algorithm(QgsProcessingAlgorithm):
     INPUT = 'INPUT'
     OUTPUT = 'OUTPUT'
