2d7c3269f1234db7e3a08f50db01239f1b333c88
novotny-marek <ahoj@mnovotny.xyz>
2025-06-29 13:56:39 +0200

    utils: Drop deprecated function

--- momeq/utils.py
+++ momeq/utils.py
@@ -2,29 +2,6 @@ import geopandas as gpd
 import shapely as shp
 
 
-def to_gdf(layer):
-    """Convert QGIS layer to GeoDataFrame"""
-    features = []
-    for feature in layer.getFeatures():
-        geom = feature.geometry()
-        if geom:
-            # Convert QGIS geometry to Shapely geometry
-            geom_wkt = geom.asWkt()
-            shapely_geom = shp.wkt.loads(geom_wkt)
-
-            features.append(
-                {
-                    "geometry": shapely_geom,
-                    **{field.name(): feature[field.name()] for field in layer.fields()},
-                }
-            )
-
-    if features:
-        return gpd.GeoDataFrame(features, crs=layer.sourceCrs().authid())
-    else:
-        return gpd.GeoDataFrame()
-
-
 def qgs_to_gpd(source, attribute_fields=None):
     """
     Convert QGIS feature soure to Geopandas GeoSeries
